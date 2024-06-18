d7625c86280f83809af5c7c736c84508352b83c3
Saplitsyn Denis <packman17@mail.ru>
2024-06-18 20:29:17 +0300

    add welcome controller with simple test

--- src/test/java/hexlet/code/AppApplicationTests.java
+++ src/test/java/hexlet/code/AppApplicationTests.java
@@ -1,13 +1,33 @@
 package hexlet.code;
 
 import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class AppApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	public void testWelcome() throws Exception {
+		var result = mockMvc.perform(get("/welcome"))
+				.andExpect(status().isOk())
+				.andReturn();
+
+		var body = result.getResponse().getContentAsString();
+		assertThat(body).contains("Welcome to Spring");
 	}
 
 }
+
