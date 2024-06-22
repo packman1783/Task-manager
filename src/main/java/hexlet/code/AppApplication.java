4e43e6c1547966bc9603e69dbb6f06a2b3adcf08
Saplitsyn Denis <packman17@mail.ru>
2024-06-22 20:08:51 +0300

    add bean faker

--- src/main/java/hexlet/code/AppApplication.java
+++ src/main/java/hexlet/code/AppApplication.java
@@ -1,8 +1,10 @@
 package hexlet.code;
 
+import net.datafaker.Faker;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -13,4 +15,8 @@ public class AppApplication {
         SpringApplication.run(AppApplication.class, args);
     }
 
+    @Bean
+    public Faker getFaker() {
+        return new Faker();
+    }
 }
