441b0bb90daf81a1241626374b1dd0e6598ee437
Saplitsyn Denis <packman17@mail.ru>
2024-06-22 20:24:11 +0300

    add util files

---
+++ src/main/java/hexlet/code/util/UserUtils.java
@@ -0,0 +1,27 @@
+package hexlet.code.util;
+
+import hexlet.code.exception.ResourceNotFoundException;
+import hexlet.code.model.User;
+import hexlet.code.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserUtils {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User getCurrentUser() {
+        var authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return null;
+        }
+
+        var email = authentication.getName();
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
+    }
+}
