cb1f5d987418771111f728e8345a625adb68de01
Saplitsyn Denis <packman17@mail.ru>
2024-07-01 15:15:35 +0300

    add  task status service

---
+++ src/main/java/hexlet/code/service/TaskStatusService.java
@@ -0,0 +1,54 @@
+package hexlet.code.service;
+
+import hexlet.code.dto.taskStatus.TaskStatusCreateDTO;
+import hexlet.code.dto.taskStatus.TaskStatusDTO;
+import hexlet.code.dto.taskStatus.TaskStatusUpdateDTO;
+import hexlet.code.exception.ResourceNotFoundException;
+import hexlet.code.mapper.TaskStatusMapper;
+import hexlet.code.repository.TaskStatusRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskStatusService {
+    @Autowired
+    private TaskStatusRepository taskStatusRepository;
+
+    @Autowired
+    private TaskStatusMapper taskStatusMapper;
+
+    public List<TaskStatusDTO> getAll() {
+        var taskStatuses = taskStatusRepository.findAll();
+        return taskStatuses.stream()
+                .map(taskStatusMapper::map)
+                .toList();
+    }
+
+    public TaskStatusDTO getById(long id) {
+        var taskStatus = taskStatusRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Task status with id " + id + " not found"));
+        return taskStatusMapper.map(taskStatus);
+    }
+
+    public TaskStatusDTO create(TaskStatusCreateDTO data) {
+        var taskStatus = taskStatusMapper.map(data);
+        taskStatusRepository.save(taskStatus);
+        return taskStatusMapper.map(taskStatus);
+    }
+
+    public TaskStatusDTO update(TaskStatusUpdateDTO data, Long id) {
+        var taskStatus = taskStatusRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Task status with id " + id + " not found"));
+
+        taskStatusMapper.update(data, taskStatus);
+        taskStatusRepository.save(taskStatus);
+        return taskStatusMapper.map(taskStatus);
+    }
+
+    public void delete(Long id) {
+        taskStatusRepository.deleteById(id);
+    }
+}
