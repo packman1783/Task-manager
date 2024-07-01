80d08c5579ad284cc1b2342cfd6d887e6b83b778
Saplitsyn Denis <packman17@mail.ru>
2024-07-01 15:11:54 +0300

    add task statuses

--- src/main/java/hexlet/code/component/DataInitializer.java
+++ src/main/java/hexlet/code/component/DataInitializer.java
@@ -1,8 +1,11 @@
 package hexlet.code.component;
 
+import hexlet.code.dto.taskStatus.TaskStatusCreateDTO;
 import hexlet.code.model.User;
 import hexlet.code.repository.UserRepository;
 import hexlet.code.service.CustomUserDetailsService;
+import hexlet.code.mapper.TaskStatusMapper;
+import hexlet.code.repository.TaskStatusRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
@@ -10,6 +13,8 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 import lombok.AllArgsConstructor;
+
+import java.util.Map;
 
 @Component
 @AllArgsConstructor
@@ -20,6 +25,12 @@ public class DataInitializer implements ApplicationRunner {
 
     @Autowired
     private final CustomUserDetailsService userService;
+
+    @Autowired
+    private final TaskStatusRepository taskStatusRepository;
+
+    @Autowired
+    private final TaskStatusMapper taskStatusMapper;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
@@ -32,5 +43,22 @@ public class DataInitializer implements ApplicationRunner {
             userData.setEncryptedPassword(password);
             userService.createUser(userData);
         }
+
+        var defaultSlugs = Map.of(
+                "draft", "Draft",
+                "to_review", "To review",
+                "to_be_fixed", "To be fixed",
+                "to_publish", "To publish",
+                "published", "Published"
+        );
+
+        for (var entry : defaultSlugs.entrySet()) {
+            var taskStatusCreateDTO = new TaskStatusCreateDTO();
+            taskStatusCreateDTO.setSlug(entry.getKey());
+            taskStatusCreateDTO.setName(entry.getValue());
+            var taskStatus = taskStatusMapper.map(taskStatusCreateDTO);
+            taskStatusRepository.save(taskStatus);
+        }
+
     }
 }
