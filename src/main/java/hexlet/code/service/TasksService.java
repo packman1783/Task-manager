849f960d6673e79ddc96bf472bbeae83ba9f1837
Saplitsyn Denis <packman17@mail.ru>
2024-07-04 00:41:39 +0300

    add service for Task

---
+++ src/main/java/hexlet/code/service/TasksService.java
@@ -0,0 +1,61 @@
+package hexlet.code.service;
+
+import hexlet.code.dto.task.TaskCreateDTO;
+import hexlet.code.dto.task.TaskUpdateDTO;
+import hexlet.code.exception.ResourceNotFoundException;
+import hexlet.code.dto.task.TaskDTO;
+import hexlet.code.mapper.TaskMapper;
+import hexlet.code.repository.TaskRepository;
+import hexlet.code.repository.TaskStatusRepository;
+import hexlet.code.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TasksService {
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Autowired
+    private TaskStatusRepository taskStatusRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TaskMapper taskMapper;
+
+    public List<TaskDTO> getAll() {
+        var tasks = taskRepository.findAll();
+        return tasks.stream()
+                .map(taskMapper::map)
+                .toList();
+    }
+
+    public TaskDTO getById(long id) {
+        var task = taskRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Task with id " + id + " not found"));
+        return taskMapper.map(task);
+    }
+
+    public TaskDTO create(TaskCreateDTO data) {
+        var task = taskMapper.map(data);
+        taskRepository.save(task);
+        return taskMapper.map(task);
+    }
+
+    public TaskDTO update(TaskUpdateDTO data, Long id) {
+        var task = taskRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Task with id " + id + " not found"));
+        taskMapper.update(data, task);
+        taskRepository.save(task);
+        return taskMapper.map(task);
+    }
+
+    public void delete(Long id) {
+        taskRepository.deleteById(id);
+    }
+}
