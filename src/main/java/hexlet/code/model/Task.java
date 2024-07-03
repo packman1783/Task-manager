b8a7a2ade4a5684e173f8d4940131e1063b59ee5
Saplitsyn Denis <packman17@mail.ru>
2024-07-04 00:42:21 +0300

    add model for Task

---
+++ src/main/java/hexlet/code/model/Task.java
@@ -0,0 +1,57 @@
+package hexlet.code.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tasks")
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString(onlyExplicitlyIncluded = true)
+public class Task implements BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @NotBlank
+    @Column(unique = true)
+    private String name;
+
+    private long index;
+
+    @ToString.Include
+    private String description;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.EAGER)
+    private TaskStatus taskStatus;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    private User assignee;
+
+    @CreatedDate
+    private LocalDate createdAt;
+}
