8d20cd858b6188e6e71a8c9fe6514cee790c975e
Saplitsyn Denis <packman17@mail.ru>
2024-07-05 15:20:33 +0300

    refactor task service with adding task specification

--- src/main/java/hexlet/code/service/TasksService.java
+++ src/main/java/hexlet/code/service/TasksService.java
@@ -1,13 +1,16 @@
 package hexlet.code.service;
 
 import hexlet.code.dto.task.TaskCreateDTO;
+import hexlet.code.dto.task.TaskParamsDTO;
 import hexlet.code.dto.task.TaskUpdateDTO;
 import hexlet.code.exception.ResourceNotFoundException;
 import hexlet.code.dto.task.TaskDTO;
 import hexlet.code.mapper.TaskMapper;
+import hexlet.code.repository.LabelRepository;
 import hexlet.code.repository.TaskRepository;
 import hexlet.code.repository.TaskStatusRepository;
 import hexlet.code.repository.UserRepository;
+import hexlet.code.specification.TaskSpecification;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,20 +19,28 @@ import java.util.List;
 
 @Service
 public class TasksService {
-    @Autowired
-    private TaskRepository taskRepository;
 
     @Autowired
-    private TaskStatusRepository taskStatusRepository;
+    private TaskRepository taskRepository;
 
     @Autowired
     private UserRepository userRepository;
 
     @Autowired
+    private TaskStatusRepository taskStatusRepository;
+
+    @Autowired
     private TaskMapper taskMapper;
 
-    public List<TaskDTO> getAll() {
-        var tasks = taskRepository.findAll();
+    @Autowired
+    private LabelRepository labelRepository;
+
+    @Autowired
+    private TaskSpecification taskSpecification;
+
+    public List<TaskDTO> getAll(TaskParamsDTO params) {
+        var specification = taskSpecification.build(params);
+        var tasks = taskRepository.findAll(specification);
         return tasks.stream()
                 .map(taskMapper::map)
                 .toList();
