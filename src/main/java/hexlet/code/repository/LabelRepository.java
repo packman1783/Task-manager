8bc28b8edfcb83f441540961fd7d74c5a09c8bbf
Saplitsyn Denis <packman17@mail.ru>
2024-07-09 14:16:12 +0300

    add method

--- src/main/java/hexlet/code/repository/LabelRepository.java
+++ src/main/java/hexlet/code/repository/LabelRepository.java
@@ -4,5 +4,8 @@ import hexlet.code.model.Label;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LabelRepository extends JpaRepository<Label, Long> {
+    Optional<Label> findByName(String name);
 }
